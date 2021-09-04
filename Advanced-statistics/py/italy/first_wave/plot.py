aa30017ae3a23c51188cefde452359eaf56e1b38
Nuelam9 <Nuelam9@github.com>
2021-09-04 15:35:47 +0200

    some changes

--- Advanced-statistics/py/italy/first_wave/plot.py
+++ Advanced-statistics/py/italy/first_wave/plot.py
@@ -3,8 +3,8 @@
 import glob
 import numpy as np
 import pandas as pd
+from datetime import timedelta
 import sys
-
 sys.path.append('../../../modules/')
 from utils import get_data
 import matplotlib.pyplot as plt
@@ -12,25 +12,44 @@ import matplotlib.pyplot as plt
 
 class Plot_solutions:
 
-    def __init__(self, tf):
-        self.tf = tf
+    def __init__(self, samples):
+        self.samples = samples
 
-    def sol_plot(self, file_path: str, plot_path: str, 
+    def sol_plot(self, file_path: str, plot_path: str, lin: bool,
                  dates: pd.core.indexes.datetimes.DatetimeIndex,
-                 t0: int, tf: int) -> None:
-        sol = np.load(file_path)[:, :10]
+                 sol_dates: pd.core.indexes.datetimes.DatetimeIndex) -> None:
+        
+        sol = np.load(file_path) # [:, :10]
         var = file_path.split('_')[-2]
+        t0 = samples['t0'] - 1
+        tf = samples['tf']
+
+        if not lin:
+            sol = np.log(sol) / np.log(10)
+            mode = 'log'
+            if var in ['I', 'X']:
+                obs = samples[var][:tf] / np.log(10)
+                plt.plot(dates[:tf], obs, 'ro', label='Observed data')
+                plt.legend()      
+        else:
+            mode = 'lin'
+            if var in ['I', 'X']:
+                obs = np.exp(samples[var][:tf])
+                plt.scatter(dates[:tf], obs, c='r', label='Observed data')
+                plt.legend()   
+            
         ci = 95.
         sol1 = np.percentile(sol, (100. + ci) / 2., axis=1)
         sol2 = np.percentile(sol, (100. - ci) / 2., axis=1)
         sol_med = np.median(sol, axis=1)
-        t = np.arange(0, self.tf, 0.01)
-        plt.fill_between(t, sol1, sol2, color='orange', alpha=0.3)
-        plt.plot(t, sol_med, color='orange')
-        #plt.xticks(dates[np.arange(t0, tf, 20)])
+
+        plt.fill_between(sol_dates, sol1, sol2, color='orange', alpha=0.3)
+        plt.plot(sol_dates, sol_med, color='orange')
+        plt.xticks(sol_dates[np.arange(t0, tf * 100 , 20*100)])
         plt.ylabel(f'${var}(t)$', fontsize=14, weight='bold')
-        plt.savefig(plot_path + f'plot_{var}.png', dpi=400)#transparent=True, dpi=400)
-        plt.show()
+        plt.grid()
+        plt.savefig(plot_path + f'plot_{mode}_{var}.png', dpi=400)#transparent=True, dpi=400)
+        # plt.show()
         plt.clf()
         plt.close()
 
@@ -47,9 +66,19 @@ if __name__ == '__main__':
     files = glob.glob1(sol_path, '*.npy')
     dates = pd.to_datetime(samples['date'],
                            format='%Y.%m.%d').dt.strftime('%d/%m/%Y')
+    step = 0.01
+    start = dates.iloc[0]
+    end = dates.iloc[samples['tf']]
+    dates = pd.date_range(start, end, freq='D')[:-1]
+    frac = 86400. * step  # Seconds corresponding to Day / (1 / step)
+    sol_dates = pd.date_range(start, end, freq=f'{frac}S')[:-1]
 
-    plot_sol = Plot_solutions(tf = samples['tf'])
+    plot_sol = Plot_solutions(samples)
 
-    for file in files:
-        plot_sol.sol_plot(sol_path + file, path + 'plot/ode_sol/',
-                          dates, t0=samples['t0'] - 1, tf = samples['tf'] - 1)
+    for lin in [True, False]:
+        for file in files:
+            plot_sol.sol_plot(sol_path + file, 
+                            path + 'plot/ode_sol/',
+                            lin,
+                            dates, 
+                            sol_dates)
